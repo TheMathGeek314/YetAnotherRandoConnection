090f4aab857eab54787fb74daec3af0f2e479e8b
TheMathGeek_314 <16294237+TheMathGeek314@users.noreply.github.com>
2026-06-01 13:51:01 -0500

    Version bump

--- YetAnotherRandoConnection.cs
+++ YetAnotherRandoConnection.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 namespace YetAnotherRandoConnection {
     public class YetAnotherRandoConnection: Mod, IGlobalSettings<GlobalSettings> {
         new public string GetName() => "YetAnotherRandoConnection";
-        public override string GetVersion() => "1.2.2.2";
+        public override string GetVersion() => "1.2.2.3";
 
         public static GlobalSettings Settings { get; set; } = new();
         public void OnLoadGlobal(GlobalSettings s) => Settings = s;
